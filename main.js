34594f71d17fee3966ca042bf7a05f6b04962585
080812 <74765678+080812@users.noreply.github.com>
2021-10-11 15:44:45 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,14 +1,34 @@
+NoseX=0;
+NoseY=0;
+
 function preload(){
 
 }
 
 function setup(){
-  canvas=createCanvas(300,300)  
+  canvas=createCanvas(500,500)  
   canvas.center()
   video=createCapture(VIDEO)
+  video.size(500,500)
 
+  poseNet=ml5.poseNet(video,modelLoaded)
+  poseNet.on('pose',gotposes)
 }
 
 function draw(){
+background('#00FFFF')
+text('Tanu', NoseX,NoseY)
+}
 
+function modelLoaded(){
+  console.log('model is loaded')
+}
+
+function gotposes(results){
+  if(results.length>0){
+    console.log(results)
+
+    NoseX=results[0].pose.nose.x 
+    NoseY=results[0].pose.nose.y
+  }
 }
